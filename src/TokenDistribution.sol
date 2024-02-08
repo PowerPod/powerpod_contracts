c3cb5ff2ed3265295d2fb55aaf703f7be4d5d391
bob <>
2024-02-08 22:15:22 +0800

    imporve token distribution contract

--- src/TokenDistribution.sol
+++ src/TokenDistribution.sol
@@ -4,20 +4,36 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
-contract TokenDistribution is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+contract TokenDistribution is 
+    Initializable, 
+    OwnableUpgradeable, 
+    UUPSUpgradeable, 
+    ReentrancyGuardUpgradeable 
+{
     IERC20 public PPDToken;
 
     uint256 public constant INITIAL_PRODUCTION = 45 * 10**6 * 10**18;
-    uint256 public productionRate;
-    uint256 public lastHalvingTime;
     uint256 public constant HALVING_PERIOD = 4 * 365 days;
+    uint32 public constant PERIOD_LENGTH_IN_SECONDS = 12 * 60 * 60;
+    uint256 public lastHalvingTime;
+    uint256 public startTime;
 
-    mapping(address => uint256) public investments;
+    uint256 public annualAllocation;
+    uint256 public dailyAllocation;
+    uint256 public periodAllocation;
+    
+    mapping(uint32 => mapping(address => uint256)) public investments;
+    mapping(uint32 => uint256) public totalInvestedInPeriod;
+    mapping(uint32 => mapping(address => bool)) public rewardClaimed;
 
-    event Invested(address investor, uint256 amount);
+    mapping(uint32 => bool) public periodBurned;
+
+    event Invested(address investor, uint32 period, uint256 amount);
+    event TokensClaimed(address investor, uint32 period, uint256 amount);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -27,27 +43,83 @@ contract TokenDistribution is Initializable, OwnableUpgradeable, UUPSUpgradeable
     function initialize(address _PPDTokenAddress) public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
 
         PPDToken = IERC20(_PPDTokenAddress);
 
-        productionRate = INITIAL_PRODUCTION;
+        annualAllocation = INITIAL_PRODUCTION * 125 / 1000;
+        dailyAllocation = annualAllocation / 365;
+        periodAllocation = dailyAllocation / 2;
+
         lastHalvingTime = block.timestamp;
+        
+        uint256 currentTimestamp = block.timestamp;
+        uint256 secondsUntilNextHour = 3600 - (currentTimestamp % 3600);
+        startTime = currentTimestamp + secondsUntilNextHour;
     }
 
-    function halveProduction() public onlyOwner {
+    function getCurrentPeriod() public view returns (uint32) {
+        require(block.timestamp >= startTime, "Distribution has not started yet");
+
+        uint256 timeSinceLastHalving = block.timestamp - startTime;
+        uint32 periodsSinceLastHalving = uint32(timeSinceLastHalving / PERIOD_LENGTH_IN_SECONDS);
+
+        return periodsSinceLastHalving + 1;
+    }
+
+    function halveProduction() public {
         require(block.timestamp >= lastHalvingTime + HALVING_PERIOD, "Halving period not reached");
-        productionRate /= 2;
+        
+        annualAllocation = annualAllocation / 2;
+        dailyAllocation = annualAllocation / 365;
+        periodAllocation = dailyAllocation / 2;
+
         lastHalvingTime = block.timestamp;
     }
 
-    function investPT(uint256 amount) public {
-        investments[msg.sender] += amount;
-        emit Invested(msg.sender, amount);
+    function investPT(uint32 _period, uint256 _amount) public {
+        require(_amount > 0, "Investment amount must be greater than 0");
+
+        uint32 currentPeriod = getCurrentPeriod();
+        require(_period == currentPeriod, "Invalid investment period");
+
+        investments[_period][msg.sender] += _amount;
+
+        emit Invested(msg.sender, _period, _amount);
     }
 
-    function distributeTokens() public {
-        // Logic to distribute tokens based on investments every 12 hours
-        // If no investments, burn the tokens instead
+    function claimTokens(uint32 _period) public nonReentrant{
+        require(!rewardClaimed[_period][msg.sender], "Reward already claimed");
+
+        uint256 periodEndTime = startTime + (_period * PERIOD_LENGTH_IN_SECONDS);
+        require(block.timestamp > periodEndTime, "Period has not ended yet");
+
+        uint256 invested = investments[_period][msg.sender];
+        require(invested > 0, "No investment in period");
+
+        uint256 totalInvested = totalInvestedInPeriod[_period];
+        uint256 reward = (periodAllocation * invested) / totalInvested;
+
+        require(reward > 0, "No tokens to claim");
+        rewardClaimed[_period][msg.sender] = true;
+
+        PPDToken.transfer(msg.sender, reward);
+        emit TokensClaimed(msg.sender, _period, reward);
+    }
+
+    function burnUninvestedPeriodTokens(uint32 _period) public {
+        uint256 periodEndTime = startTime + (_period * PERIOD_LENGTH_IN_SECONDS);
+        require(block.timestamp > periodEndTime, "Period has not ended yet");
+
+        require(!periodBurned[_period], "Period already burned");
+
+        uint256 totalInvested = totalInvestedInPeriod[_period];
+        require(totalInvested == 0, "Period has investments");
+        
+        periodBurned[_period] = true;
+
+        uint256 tokensToBurn = periodAllocation;
+        PPDToken.transfer(address(0x0), tokensToBurn);
     }
 
     function _authorizeUpgrade(
